442baa97ccff3cedbc2fe53e0f1d85bb1229f40c
RidRan <ridranmc@gmail.com>
2021-12-21 02:41:39 -0600

    Updated UI, code editing, and add test text input

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public string test;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            GameObject outputTextBox = GetOutputTextBox();
+            GameObject inputTextBox = GetInputTextBox();
+            string input = inputTextBox.GetComponent<TMP_InputField>().text;
+            if (input != "")
+            {
+                outputTextBox.GetComponent<TextMeshProUGUI>().text = outputTextBox.GetComponent<TextMeshProUGUI>().text + "> " + input + "\n";
+                inputTextBox.GetComponent<TMP_InputField>().text = "";
+            }
+            inputTextBox.GetComponent<TMP_InputField>().Select();
+            inputTextBox.GetComponent<TMP_InputField>().ActivateInputField();
+        }
+    }
+
+    GameObject GetInputTextBox()
+    {
+        return gameObject.transform.GetChild(1).gameObject;
+    }
+
+    GameObject GetOutputTextBox()
+    {
+        return gameObject.transform.GetChild(5).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+    }
+}
